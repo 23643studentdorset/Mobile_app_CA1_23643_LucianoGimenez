e7c18c073d36259071d3f000dc22bec23c542375
23643studentdorset <23643@student.dorset-college.ie>
2022-03-03 23:35:29 +0000

    skills page with names and icons

--- app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/skills.kt
+++ app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/skills.kt
@@ -1,13 +1,12 @@
 package com.luciano.mobile_app_ca1_23643_lucianogimenez
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Education:AppCompatActivity() {
+class skills:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.education)
+        setContentView(R.layout.skills)
 
     }
 
