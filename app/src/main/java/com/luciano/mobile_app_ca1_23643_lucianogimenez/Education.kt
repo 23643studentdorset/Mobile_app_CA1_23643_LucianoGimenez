5b68e34ce83f69d86861805de815f44318894ad8
23643studentdorset <23643@student.dorset-college.ie>
2022-03-03 20:53:11 +0000

    Initial commit

---
+++ app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/Education.kt
@@ -0,0 +1,14 @@
+package com.luciano.mobile_app_ca1_23643_lucianogimenez
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Education:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.education)
+
+    }
+
+}
---
+++ app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.luciano.mobile_app_ca1_23643_lucianogimenez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("checker", "starting")
+
+    }
+}
