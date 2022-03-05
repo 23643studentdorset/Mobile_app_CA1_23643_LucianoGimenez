881655a857fe86d82b4f3763e3fed8bafc912f0c
23643studentdorset <23643@student.dorset-college.ie>
2022-03-05 22:08:42 +0000

    Education page

---
+++ app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/Hobbies.kt
@@ -0,0 +1,14 @@
+package com.luciano.mobile_app_ca1_23643_lucianogimenez
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class Hobbies:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hobbies)
+
+    }
+
+}
