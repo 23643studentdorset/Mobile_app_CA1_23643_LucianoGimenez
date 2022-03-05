6c8a6dd4fa1ef653ec1f16aa9547689e90360761
23643studentdorset <23643@student.dorset-college.ie>
2022-03-05 15:50:30 +0000

    Education page

---
+++ app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/Skills.kt
@@ -0,0 +1,13 @@
+package com.luciano.mobile_app_ca1_23643_lucianogimenez
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Skills:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.skills)
+
+    }
+
+}
