5060cd095f583be9d6beed5b70895d6c5f63d3eb
23643studentdorset <23643@student.dorset-college.ie>
2022-03-06 22:30:55 +0000

    starting main_activity behaviour

--- app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/MainActivity.kt
+++ app/src/main/java/com/luciano/mobile_app_ca1_23643_lucianogimenez/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.luciano.mobile_app_ca1_23643_lucianogimenez
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.i("checker", "starting")
 
+        val closeButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.closeButton)
+        val educationButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.educationButton)
+        val skillsButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.skillsButton)
+        val hobbiesButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.hobbiesButton)
+        val achievementsButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.achievementsButton)
+
+        closeButton.setOnClickListener {
+            finishActivity()
+        }
+        educationButton.setOnClickListener {
+            val educationActivity = Intent(this, Education::class.java)
+            startActivity(educationActivity)
+        }
+        skillsButton.setOnClickListener {
+        }
+        hobbiesButton.setOnClickListener {
+        }
+        achievementsButton.setOnClickListener {
+        }
+
+    }
+
+    private fun finishActivity() {
+        finish()
     }
 }
